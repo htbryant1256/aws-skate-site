a90bbd7c4afb7bb5de8cef25a00c78e3e5984ef8
Henry Bryant <htbryant1256@eagle.fgcu.edu>
2022-12-05 23:37:08 -0500

    Added GraphQL and Dynamo DB

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "awsskatesite": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
